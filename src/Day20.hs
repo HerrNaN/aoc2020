8fd7f132ceac6ba02fd7b77919ea638efaac3ce2
Karl 'NaN' Wikström <karl@wikstrom.io>
2020-12-21 17:44:53 +0100

    Add better solution to Day20Part1

--- src/Day20.hs
+++ src/Day20.hs
@@ -22,12 +22,12 @@ import Data.Text (dropEnd)
 import Data.Maybe
 import Linear hiding (transpose)
 import Control.Lens
+import Prelude hiding (Right, Left)
 
 type Point = V2 Int
-type Dims  = (Int, Int)
 type Border = String
 type Contents = [String]
-data Transformation = None | Rot90 | Rot180 | Rot270 | VFlip | HFlip
+data Transformation = None | Rot90 | Rot180 | Rot270 | VFlip | HFlip | Rot90HFlip | Rot90VFlip
     deriving (Eq, Ord, Enum, Show)
 type Puzzle = Map Point Contents
 type Image  = [String]
@@ -37,14 +37,34 @@ data Piece = P
     , pContents :: Map Transformation Contents
     } deriving (Eq, Show)
 
+data PPiece = PP
+    { ppId :: PID
+    , ppEdges :: Set Edge
+    , ppContents :: Contents
+    } deriving (Eq, Show)
+type Edge = (Side, Border)
 
-type IPiece = (Int, [Int])
+data Side = Up | Down | Right | Left
+    deriving (Show, Ord, Eq, Enum)
 type PID = Int
+type ConnectionMap = Map Edge (Set Piece)
+
+opp :: Side -> Side
+opp Up    = Down
+opp Down  = Up
+opp Left  = Right
+opp Right = Left
 
 monster :: [String]
 monster = [ "                  # "
           , "#    ##    ##    ###"
           , " #  #  #  #  #  #   " ]
+
+-- With a center point on the end of the tail
+monsterCoords :: [V2 Integer]
+monsterCoords = [V2 18 1 
+                ,V2 0 0, V2 5 0, V2 6 0, V2 11 0, V2 12 0, V2 17 0, V2 18 0, V2 19 0
+                ,V2 1 1, V2 4 1, V2 7 1, V2 10 1, V2 13 1, V2 16 1]
 
 count :: Eq a => a -> [a] -> Int
 count a = countTrue (==a)
@@ -52,13 +72,13 @@ count a = countTrue (==a)
 day20a :: String -> Int
 day20a = solveA . dayInput
 
-solveA :: [Piece] -> Int
-solveA = product . corners
+solveA :: [PPiece] -> Int
+solveA = product . corners'
 
 day20b :: String -> Int
 day20b = solveB . dayInput
 
-solveB :: [Piece] -> Int
+solveB :: [PPiece] -> Int
 solveB ps = ws - (nm * mws)
     where img = toImage $ assemble ps
           nm  = countMonsters img
@@ -68,7 +88,7 @@ solveB ps = ws - (nm * mws)
 countMonsters :: Image -> Int
 countMonsters = error "not implemented"
 
-assemble :: [Piece] -> Puzzle
+assemble :: [PPiece] -> Puzzle
 assemble = error "not implemented"
 
 borderMap :: [Piece] -> Map Border [PID]
@@ -124,27 +144,47 @@ linkX ps = foldl (zipWith (++)) (repeat "") css
   four times in total (each). Then we just get the actual ids of
   the pieces.
 -}
-corners :: [Piece] -> [PID]
-corners = map head
-        . filter ((== 4) . length)
-        . group
-        . sort
-        . concat
-        . M.elems
-        . M.filter ((== 1) . length)
-        . borderMap
+-- corners :: [PPiece] -> [PID]
+-- corners = map head
+--         . filter ((== 4) . length)
+--         . group
+--         . sort
+--         . concat
+--         . M.elems
+--         . M.filter ((== 1) . length)
+--         . borderMap
+
+corners' :: [PPiece] -> [PID]
+corners' pps = S.toList $ S.fromList $ map ppId $ filter (isCorner em) pps
+    where em = edgeMap pps
+
+isCorner :: Map Edge [PID] -> PPiece -> Bool
+isCorner em pp = 2 == numberOfUsableEdges em pp
+
+numberOfUsableEdges :: Map Edge [PID] -> PPiece -> Int
+numberOfUsableEdges em PP{..} = length $ S.filter (\(s,b) -> length (em M.! (opp s, b)) > 1) ppEdges
+
+edgeMap :: [PPiece] -> Map Edge [PID]
+edgeMap = foldl addEdges M.empty
+
+addEdges :: Map Edge [PID] -> PPiece -> Map Edge [PID]
+addEdges em PP{..} = M.unionWith (++) em $ M.fromList $ zip (S.elems ppEdges) $ repeat [ppId]
 
 -- This conversion shouldn't be neccesary but helps alot with debugging 
 borderToInt :: Border -> Int
 borderToInt = fromInteger . fromBin . map (\c -> if c == '#' then '1' else '0')
 
-dayInput :: String -> [Piece]
+dayInput :: String -> [PPiece]
 dayInput i = ps'
     where ps = splitOn "\n\n" (T.unpack (T.strip $ T.pack i))
-          ps' = map (parsePiece . lines) ps
+          ps' = concatMap (parsePPieces . lines) ps
 
 parsePiece :: [String] -> Piece
 parsePiece (l:ls) = mkPiece pId ls
+    where pId = unsafeParse pid l
+
+parsePPieces :: [String] -> [PPiece]
+parsePPieces (l:ls) = fromTemplate pId ls
     where pId = unsafeParse pid l
           
 stripBorders :: [String] -> [String]
@@ -159,6 +199,24 @@ mkPiece pId cs = P
     where cm = M.fromList $ zip [None ..] $ map (`doTransform` cs) [None ..]
           bm = M.map (S.fromList . borders) cm
 
+fromTemplate :: PID -> Contents -> [PPiece]
+fromTemplate pId cs = map (mkPPiece pId cs) [None ..]
+
+mkPPiece :: PID -> Contents -> Transformation -> PPiece
+mkPPiece pId cs t = PP pId es cs'
+    where cs' = doTransform t cs
+          es  = edges cs'
+
+edges :: Contents -> Set Edge
+edges cs = S.fromList $ map (\s -> (s, borderFrom cs s)) [Up ..]
+
+borderFrom :: Contents -> Side ->  Border
+borderFrom cs = \case
+    Up -> head cs
+    Down -> last cs
+    Left -> head $ transpose cs
+    Right -> last $ transpose cs
+
 doTransform :: Transformation -> Contents -> Contents
 doTransform None   = id
 doTransform Rot90  = (!! 1) . iterate rotCw90
@@ -166,6 +224,8 @@ doTransform Rot180 = (!! 2) . iterate rotCw90
 doTransform Rot270 = (!! 3) . iterate rotCw90
 doTransform HFlip  = reverse
 doTransform VFlip  = map reverse
+doTransform Rot90HFlip = doTransform HFlip . doTransform Rot90
+doTransform Rot90VFlip = doTransform VFlip . doTransform Rot90
 
 possibleBorders :: [Border] -> [Border]
 possibleBorders ss = map reverse bs ++ bs
